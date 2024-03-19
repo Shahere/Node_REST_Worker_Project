32527927cabcf4b7689f5a6dd34bce9076b40d01
Savinien <savinienbarbotaud@gmail.com>
2024-03-19 16:02:43 +0100

    auth2

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,10 +1,22 @@
 const { WorkersService } = require('../use-cases/WorkersService')
 const { PortExplorer } = require('../use-cases/PortExplorer')
+const jwt = require("jsonwebtoken")
+
+const SECRET_TO_HIDE_ENV_FILE = "YGD*m$$Pb2*45U#9f8DF%@@85X#456e3cyyQr&@B&"
 
 function getToken(req, res, next) {
+    try {
+        let username = req.body.username;
+        let password = req.body.password;
 
+        //TODO verify
+        return jwt.sign({"username":username,"password":password},PERSON_API_ACCES_TOKEN_SECRET , {expiresIn: "150m"}) 
+    } catch (error) {
+        console.error(`>>> ${error} ${error.stack}`)
+        res.status(500).send('Internal Server Error')
+    }
 }
 
 module.exports = {
-  getToken
+    getToken
 }
