3833cbc399d4ce4d3dd2cd36d96ec7a8da835f76
Savinien <savinienbarbotaud@gmail.com>
2024-03-20 14:49:18 +0100

    updtae roles

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -7,14 +7,22 @@ function getToken(req, res, next) {
         let username = req.body.username;
         let password = req.body.password;
 
+        let token = null;
 
         let passwordauth = process.env.PASSWORDAUTH
         let usernameauth = process.env.USERNAMEAUTH
 
+        let passwordauthadmin = process.env.PASSWORDAUTHADMIN
+        let usernameauthadmin = process.env.USERNAMEAUTHADMIN
+
         if (username == usernameauth && password == passwordauth) {
-            let token = jwt.sign({ "username": username, "password": password }, process.env.SECRET_KEY, { expiresIn: "150m" })
+            token = jwt.sign({ "username": username, "password": password, "role": process.env.VISITOR }, process.env.SECRET_KEY, { expiresIn: "150m" })
             res.json(token)
-        } else {
+        } else if(username == usernameauthadmin && password == passwordauthadmin) {
+            token = jwt.sign({ "username": username, "password": password, "role": process.env.ADMIN }, process.env.SECRET_KEY, { expiresIn: "150m" })
+            res.json(token)
+        }
+        else {
             console.log("False conditions")
             res.status(400).send("Can't authentificate to server")
         }
