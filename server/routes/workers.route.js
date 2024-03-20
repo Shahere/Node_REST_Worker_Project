4d4b42e5cb60c8e371a54b1d3d66b7b8d595ced9
Savinien <savinienbarbotaud@gmail.com>
2024-03-20 15:14:57 +0100

    token client and middle ware

--- server/routes/workers.route.js
+++ server/routes/workers.route.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const router = express.Router()
 const workersController = require('../controllers/workers.controller')
+const jwt = require('jsonwebtoken')
+
+router.use(function (req, res, next) {
+    console.log("Request time : " + Date.now())
+    next()
+})
+
+router.use('/', function (req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader.split(' ')[1] || authHeader
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+
+        if (err) return res.sendStatus(403)
+
+        req.user = user
+
+        next()
+    })
+})
 
 router.get('/', workersController.getWorkers)
-router.post('/',workersController.addWorker)
-router.delete('/', workersController.deleteWorker )
+router.post('/', workersController.addWorker)
+router.delete('/', workersController.deleteWorker)
 router.patch('/', workersController.updateWorker)
 
 module.exports = router
