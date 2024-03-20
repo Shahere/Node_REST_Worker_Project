1f05ca249c98171c602803058fe4b01a98e84b68
Savinien <savinienbarbotaud@gmail.com>
2024-03-20 14:41:32 +0100

    remove secret

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,8 +2,6 @@ const { WorkersService } = require('../use-cases/WorkersService')
 const { PortExplorer } = require('../use-cases/PortExplorer')
 const jwt = require("jsonwebtoken")
 
-const SECRET_TO_HIDE_ENV_FILE = "YGD*m$$Pb2*45U#9f8DF%@@85X#456e3cyyQr&@B&"
-
 function getToken(req, res, next) {
     try {
         let username = req.body.username;
